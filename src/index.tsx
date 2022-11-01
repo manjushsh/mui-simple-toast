775c01e26319a2cd5486e936f49907364e9cb748
manjushsh <manjushsh@protonmail.com>
2022-11-01 17:42:15 +0530

    Step 5 Complete

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import MUIToast from './toast/toast';
-import { ToastProvider } from './toast/toast-context';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MUIToast from './toast/toast'
+import { ToastProvider } from './toast/toast-context'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <ToastProvider>
       <MUIToast />
     </ToastProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
--- src/toast/toast.tsx
+++ src/toast/toast.tsx
@@ -1,57 +1,63 @@
-import React, { useContext, useEffect } from "react"
-import { Alert, IconButton, Snackbar, SnackbarOrigin } from "@mui/material";
-import CloseIcon from '@mui/icons-material/Close';
-import { ToastContext } from "./toast-context";
+import React, { useContext, useEffect } from 'react'
+import { Alert, IconButton, Snackbar, SnackbarOrigin } from '@mui/material'
+import CloseIcon from '@mui/icons-material/Close'
+import { ToastContext } from './toast-context'
 
 const MUISimpleToast = (props: ToastProps) => {
+  const { toast, setToast } = useContext(ToastContext)
+  const onToastClose = () =>
+    setToast({
+      show: false,
+      type: 'info',
+      message: '',
+    })
+  const SNACKBAR_CONFIG: ToastConfig = {
+    anchorOrigin: { vertical: 'bottom', horizontal: 'right' },
+    duration: 5000,
+    variant: 'filled',
+  }
 
-    const { toast, setToast } = useContext(ToastContext);
-    const onToastClose = () => setToast({
-        show: false,
-        type: 'info',
-        message: ''
-    });
-    const SNACKBAR_CONFIG: ToastConfig = {
-        anchorOrigin: { vertical: 'bottom', horizontal: 'right' },
-        duration: 5000,
-        variant: 'filled',
-    };
+  useEffect(() => {
+    setInterval(() => {
+      console.log('Setting Toast...')
+      setToast({
+        show: true,
+        type: 'error',
+        message: 'Hi There!',
+      })
+    }, props?.duration || SNACKBAR_CONFIG.duration)
+  }, [])
 
-    useEffect(() => {
-        setInterval(() => {
-            console.log("Setting Toast...");
-            setToast({
-                show: true,
-                type: 'error',
-                message: 'Hi There!'
-            })
-        }, props?.duration || SNACKBAR_CONFIG.duration)
-    }, [])
+  return (
+    <Snackbar
+      open={toast?.show}
+      autoHideDuration={props?.duration || SNACKBAR_CONFIG.duration}
+      onClose={onToastClose}
+      anchorOrigin={SNACKBAR_CONFIG.anchorOrigin}
+    >
+      <Alert
+        action={
+          <IconButton size='small' color='inherit' aria-label='close' onClick={onToastClose}>
+            <CloseIcon fontSize='inherit' />
+          </IconButton>
+        }
+        variant={props?.variant || SNACKBAR_CONFIG.variant}
+      >
+        {toast?.message || ''}
+      </Alert>
+    </Snackbar>
+  )
+}
 
-    return (
-        <Snackbar open={toast?.show} autoHideDuration={props?.duration || SNACKBAR_CONFIG.duration} onClose={onToastClose} anchorOrigin={SNACKBAR_CONFIG.anchorOrigin}>
-            <Alert
-                action={
-                    <IconButton size='small' color='inherit' aria-label='close' onClick={onToastClose}>
-                        <CloseIcon fontSize='inherit' />
-                    </IconButton>
-                }
-                variant={props?.variant || SNACKBAR_CONFIG.variant}>
-                {toast?.message || ''}
-            </Alert>
-        </Snackbar>
-    )
-};
-
-export default MUISimpleToast;
+export default MUISimpleToast
 
 export interface ToastProps {
-    variant?: 'standard' | 'filled' | 'outlined',
-    duration?: number;
-};
+  variant?: 'standard' | 'filled' | 'outlined'
+  duration?: number
+}
 
 export interface ToastConfig {
-    anchorOrigin: SnackbarOrigin;
-    duration?: number;
-    variant: ToastProps['variant'];
+  anchorOrigin: SnackbarOrigin
+  duration?: number
+  variant: ToastProps['variant']
 }
