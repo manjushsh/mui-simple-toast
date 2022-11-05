c7df3baf8e4d93d70e246b916c2b2fc63e140e70
manjushsh <dxvxgmydy@mozmail.com>
2022-11-05 15:31:46 +0530

    context

--- src/toast/toast-context.tsx
+++ src/toast/toast-context.tsx
@@ -1,36 +1,24 @@
 /* eslint-disable @typescript-eslint/no-empty-function */
 // ** React Imports
 import React, { createContext, useState, ReactNode } from 'react'
+import { defaultToast } from '..'
+import { ToastObject } from '../index.d'
 
 const defaultProvider: any = {
-  toast: {
-    show: false,
-    type: 'info',
-    message: '',
-  },
+  toast: defaultToast,
   setToast: () => {},
 }
 const ToastContext = createContext(defaultProvider)
 
 const ToastProvider = ({ children }: Props) => {
   // ** States
-  const [toast, updateToast] = useState({
-    show: false,
-    type: 'info',
-    message: '',
-  })
-  const setToast = (toastData: SetToastParams) => updateToast({ ...toast, ...toastData })
+  const [toast, updateToast] = useState(defaultToast)
+  const setToast = (toastData: ToastObject) => updateToast({ ...toast, ...toastData })
   const values = {
     toast,
     setToast,
   }
   return <ToastContext.Provider value={values}>{children}</ToastContext.Provider>
-}
-
-export interface SetToastParams {
-  show?: boolean
-  type?: 'success' | 'info' | 'warning' | 'error'
-  message?: string
 }
 
 export type Props = {
